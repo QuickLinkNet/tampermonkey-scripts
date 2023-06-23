0823d5aed3ae3108361343b553f4520444043699
DrunkenNet <33082510+DrunkenNet@users.noreply.github.com>
2023-06-23 21:07:10 +0200

    Make it fool proof

--- kleinanzeigen.de/click-on-dropdown-filter.js
+++ kleinanzeigen.de/click-on-dropdown-filter.js
@@ -1,15 +1,45 @@
 // ==UserScript==
-// @name     Kleinanzeigen Filter
-// @version  1
+// @name     Kleinanzeigen.de - distance filter
+// @version  1.1
 // @grant    none
 // @include  https://www.kleinanzeigen.de/*
+// @run-at   document-idle
 // ==/UserScript==
 
-window.addEventListener('load', function() {
-    var selectValue = document.querySelector('#sortingField-selector-value');
-    var option = document.querySelector('.selectbox-option[data-value="DISTANCE"]');
+console.log('Starting script "Distance Filter 1.1"');
 
-    if (selectValue && option && selectValue.value !== 'DISTANCE') {
-        option.click();
+function runScript() {
+    console.log('Running script "Distance Filter 1.1');
+    try {
+        var selectValue = document.querySelector('#sortingField-selector-value');
+        var option = document.querySelector('.selectbox-option[data-value="DISTANCE"]');
+
+        if (!selectValue) {
+            console.error('Could not find element with id "sortingField-selector-value"');
+            return;
+        }
+        if (!option) {
+            console.error('Could not find element with data-value "DISTANCE"');
+            return;
+        }
+
+        if (selectValue.value !== 'DISTANCE') {
+            option.click();
+            console.log('Drop-Down with option "Distance" clicked');
+        } else {
+            console.log('Drop-Down is already set to "Distance"');
+        }
+    } catch(error) {
+        console.error('An error occurred:', error);
     }
-});
+}
+
+if (document.readyState === 'loading') {
+    // Loading hasn't finished yet
+    console.log('Adding DOMContentLoaded listener...');
+    window.addEventListener('DOMContentLoaded', runScript);
+} else {
+    // `DOMContentLoaded` has already fired
+    console.log('DOMContentLoaded already fired');
+    runScript();
+}
